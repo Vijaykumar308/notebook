6eba0ca5a4efea59ef20f43584bd0cf0db9b850f
vijay kumar <308vijaykumar@gmail.com>
2023-12-25 11:05:55 +0530

    add add category in create notes desing page

--- resources/views/create_note.blade.php
+++ resources/views/create_note.blade.php
@@ -3,43 +3,51 @@
         height: 40vh;
         resize: vertical;
     }
-    /* .toast-top-center {
-        top: 38% !important;
-        margin: 0 auto;
-        left: 38%;
-    } */
 </style>
 @extends('master')
 @section('title','create')
 
 @section('content')
     <x-hero title="Notebook++ | Create"/>
-    <section class="container create-note">
-    @if(Session::has('message'))
-        <script src="https://code.jquery.com/jquery-3.7.1.min.js" integrity="sha256-/JqT3SQfawRcv/BIHPThkBvs0OEvtFFmqPF/lYI/Cxo=" crossorigin="anonymous"></script>
-        <script src="https://cdnjs.cloudflare.com/ajax/libs/toastr.js/latest/toastr.min.js" integrity="sha512-VEd+nq25CkR676O+pLBnDW09R7VQX9Mdiij052gVCp5yVH3jGtH70Ho/UUv4mJDsEdTvqRCFZg0NKGiojGnUCw==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
-        <script>
-            console.log('session working why pop not came');
-            toastr.options = {
-                "progressBar": true,
-                "closeButton": true
-            }
-            toastr.options.positionClass = 'toast-bottom-right';
-            toastr.success("{{ Session::get('message') }}",{timeOut:1000}); 
-        </script>
-    @endif
-        <form action="{{route('notes.store')}}" method="POST">
+    <section class="containersssss create-note">
+        <form action="{{route('notes.store')}}" method="POST" class="flex awesome-form">
             @csrf
-            <div class="form-group">
-                <input type="text" name="title" placeholder="Type Title" class="input-title">
-            </div>
+            <div class="left-side">
+                @if(Session::has('message'))
+                    <script src="https://code.jquery.com/jquery-3.7.1.min.js" integrity="sha256-/JqT3SQfawRcv/BIHPThkBvs0OEvtFFmqPF/lYI/Cxo=" crossorigin="anonymous"></script>
+                    <script src="https://cdnjs.cloudflare.com/ajax/libs/toastr.js/latest/toastr.min.js" integrity="sha512-VEd+nq25CkR676O+pLBnDW09R7VQX9Mdiij052gVCp5yVH3jGtH70Ho/UUv4mJDsEdTvqRCFZg0NKGiojGnUCw==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
+                    <script>
+                        console.log('session working why pop not came');
+                        toastr.options = {
+                            "progressBar": true,
+                            "closeButton": true
+                        }
+                        toastr.options.positionClass = 'toast-bottom-right';
+                        toastr.success("{{ Session::get('message') }}",{timeOut:1000}); 
+                    </script>
+                @endif
 
-            <div class="form-group">
-                <textarea cols="5" rows="15" name="notesContent" id="editor"></textarea>
-            </div>
+                <div class="form-group">
+                    <input type="text" name="title" placeholder="Type Title" class="input-title">
+                </div>
 
-            <div class="form-group">
-                <input type="submit" class="btn btn-view create-note-button" value="Create">
+                <div class="form-group">
+                    <textarea cols="5" rows="15" name="notesContent" id="editor"></textarea>
+                </div>      
+            </div>
+            <div class="right-side">
+                <div class="publish-btn-container">
+                    <input type="submit" class="btn btn-view create-note-button" value="Publish">
+                </div>
+                <div class="input-group">
+                    <select class="select-category">
+                        <option></option>
+                        <option>Blue cheese with crackers</option>
+                        <option>Steak</option>
+                        <option>Other</option>
+                    </select>
+                    <label>Select Category</label>
+                </div>
             </div>
         </form>
     </section>
@@ -56,5 +64,29 @@
                 console.error(error);
             });
     });
+
+    document.addEventListener('DOMContentLoaded', function() {
+  var inputElements = document.querySelectorAll('.awesome-form .input-group input');
+  inputElements.forEach(function(input) {
+    input.addEventListener('focusout', function() {
+      var text_val = this.value;
+      if (text_val === "") {
+        this.classList.remove('has-value');
+      } else {
+        this.classList.add('has-value');
+      }
+    });
+  });
+
+  var selectElement = document.querySelector('.awesome-form .input-group select');
+  selectElement.addEventListener('focusout', function() {
+    var text_val = this.value;
+    if (text_val === "") {
+      this.classList.remove('has-value');
+    } else {
+      this.classList.add('has-value');
+    }
+  });
+});
 </script>
 
